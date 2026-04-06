96acb2fd3cab17f43e2586510b6e3557953a46e0
vishal-karthik-haha <vv753@snu.edu.in>
2026-04-06 17:28:55 +0530

    Refactor forwarding logic in ForwardingUnit.v

--- ForwardingUnit.v
+++ ForwardingUnit.v
@@ -5,7 +5,6 @@ module ForwardingUnit (
     input  [2:0] MEM_WB_rd,
     input  [2:0] ID_EX_rs1,
     input  [2:0] ID_EX_rs2,
-
     output reg [1:0] forwardA,
     output reg [1:0] forwardB
 );
@@ -14,7 +13,6 @@ always @(*) begin
     forwardA = 2'b00;
     forwardB = 2'b00;
 
-    // EX hazard
     if (EX_MEM_reg_write && (EX_MEM_rd != 0) &&
         (EX_MEM_rd == ID_EX_rs1))
         forwardA = 2'b10;
@@ -23,7 +21,6 @@ always @(*) begin
         (EX_MEM_rd == ID_EX_rs2))
         forwardB = 2'b10;
 
-    // MEM hazard
     if (MEM_WB_reg_write && (MEM_WB_rd != 0) &&
         (MEM_WB_rd == ID_EX_rs1))
         forwardA = 2'b01;
